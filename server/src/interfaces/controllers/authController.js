73824c3fefba913c105670af988a621fbd7ba95f
SilentCodeSage <nandunandakishor345@gmail.com>
2025-07-23 19:01:38 +0530

    updated redirect url to frontend typo fix

--- server/src/interfaces/controllers/authController.js
+++ server/src/interfaces/controllers/authController.js
@@ -6,8 +6,7 @@ export const authController = {
     try {
       const user = await handleGoogleLogin(googleUser);
       //res.json({"message":"suuccess"});
-      res.redirect(`blindmate-bnef.vercel.app
-`);
+      res.redirect(`https://blindmate-bnef.vercel.app`);
     } catch (err) {
       console.log(err);
       res.status(500).json({ error: "Login failed" });
