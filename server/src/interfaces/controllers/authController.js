132749164d53616e82899b6d192e9eb9fe9f5bde
SilentCodeSage <nandunandakishor345@gmail.com>
2025-08-06 23:14:51 +0530

    session fix try(2)

--- server/src/interfaces/controllers/authController.js
+++ server/src/interfaces/controllers/authController.js
@@ -7,7 +7,7 @@ export const authController = {
       const user = await handleGoogleLogin(googleUser);
       console.log(user);
       //res.json({"message":"suuccess"});
-      res.redirect(`http://localhost:5173/`);
+      res.redirect(`${process.env.CALLBACK_URL_FRONTEND}`);
     } catch (err) {
       console.log(err);
       res.status(500).json({ error: "Login failed" });
